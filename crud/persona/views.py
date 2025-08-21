89e2b98bbc86caafcc94c034292d39fdb9250042
ivra97 <ivanxdh@gmail.com>
2025-08-20 23:23:22 -0300

    Commit de creacion de entorno virtual, crud, apps a utilizar, modelado de persona y oficina, templates, basehtml, listahtml, y algo mas

---
+++ crud/persona/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Persona
+
+
+class PersonaListView(ListView):
+    model = Persona
+    template_name = "persona/lista.html"
+    context_object_name = "personas"
+
